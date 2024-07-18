08cd6c50e49a7c46ee34fbc83b7caf855a7f791b
ArdakHere <mrglok1@gmail.com>
2024-07-18 16:28:54 +0500

    Function implemented

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-def substructure_search(mols, mol):
-    pass
+def substructure_search(mols, mol) -> list[str]:
+    substructures_searched = []
+
+    for item in mols:
+        if mol in item:
+            substructures_searched.append(item)
+
+    return substructures_searched
+
+print(substructure_search(["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"], "c1ccccc1"))
