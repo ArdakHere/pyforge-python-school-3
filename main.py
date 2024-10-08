4b9c66d34d68b21fa9929909ada231a5a1960787
ArdakHere <mrglok1@gmail.com>
2024-10-08 21:46:52 +0500

    Update requirements.txt for tracking

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from os import getenv
 
 import redis
 from fastapi import FastAPI, status, HTTPException, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from rdkit import Chem
 from fastapi.responses import JSONResponse
 
@@ -12,6 +13,17 @@ from src.molecules.router import router
 
 app = FastAPI()
 
+origins = [
+    "ec2-3-139-81-186.us-east-2.compute.amazonaws.com",  # Replace with your EC2 IP, if you're accessing directly from there
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
